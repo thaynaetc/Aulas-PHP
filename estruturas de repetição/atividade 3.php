441eee8285e54d69f59a6ed80360b43d4f72b5c1
thaynaetc <166063841+thaynaetc@users.noreply.github.com>
2024-06-26 15:26:00 -0300

    Add files via upload

---
+++ estruturas de repetição/atividade 3.php
@@ -0,0 +1,11 @@
+<?php
+ $n=5;
+ $impar;
+ $cont = 0;
+
+ while($cont < $n){
+    $impar = (2 * $cont + 1);
+    echo "{$impar}\n";
+    $cont++;
+ }
+?>
---
+++ estruturas de repetição/atividade 4.php
@@ -0,0 +1,13 @@
+<?php
+function mulTres(){
+   for($i =1; $i <= 5; $i++){ 
+    $multiplos = 3 * $i;
+    echo "{$multiplos}\n";
+   }
+
+}
+mulTres();
+
+
+
+?>
